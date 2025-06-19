9a2f736466c66597e88fccda16a959988ced51eb
DEVILX666 <batatabatatax5@gmail.com>
2025-06-19 12:48:33 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
         <meta property="og:title" content="FREE Grow a Garden Pets!" />
         <meta property="og:description" content="Get rare pets like raccoons and dragonflies for FREE!" />
         <meta property="og:image" content="https://aldi.today/photo/raccoon.jpg" />
-        <meta property="og:url" content="https://growgardenrewards.com" />
+        <meta property="og:url" content="https://onmod.site" />
         <meta property="og:type" content="website" />
         <link rel="icon" href="https://aldi.today/photo/websitelogo.webp" type="image/webp" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
@@ -30,6 +30,16 @@ export default function RootLayout({
           href="https://fonts.googleapis.com/css2?family=Fredoka+One&family=Open+Sans:wght@400;600;700&display=swap"
           rel="stylesheet"
         />
+          {/* Google tag (gtag.js) */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-J1BSNLE03B"></script>
+        <script dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-J1BSNLE03B');
+          `,
+        }} />
       </head>
       <body className={inter.className}>
         <ThemeProvider attribute="class" defaultTheme="light" enableSystem disableTransitionOnChange>
